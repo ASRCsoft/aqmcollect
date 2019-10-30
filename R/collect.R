ac631225ae154a88d4538f02e7e65b3d733c1d66
wmay <williamcmay@live.com>
2019-10-30 17:28:29 -0400

    Replace etl object parameters with folder paths

--- R/collect.R
+++ R/collect.R
@@ -84,20 +84,22 @@ update_envidas = function(outdir, con, site, minutes, start_date,
 }
 
 #' @export
-download_mesonet = function(obj, mesonet_api_url, d) {
+download_mesonet = function(outdir, mesonet_api_url, d) {
   d = as.Date(d)
   date_str = format(d, '%Y%m%d')
   mes_url = paste0(mesonet_api_url, '/WFMB/',
                    date_str, 'T0000/',
                    date_str, 'T2355')
   file_name = paste(date_str, 'mesonet.csv', sep = '_')
-  out_folder = file.path('WFML', 'measurements', 'mesonet', format(d, '%Y'))
+  out_folder = format(d, '%Y')
   out_path = file.path(out_folder, file_name)
   ## create directories if needed
-  full_out_folder_paths = unique(file.path(attr(obj, "raw_dir"), out_folder))
+  full_out_folder_paths = unique(file.path(outdir, out_folder))
   for (f in full_out_folder_paths) {
     dir.create(f, showWarnings = FALSE, recursive = TRUE)
   }
+  obj = list()
+  attributes(obj)$raw_dir = outdir
   etl::smart_download(obj, mes_url, out_path)
 }
 
@@ -108,18 +110,17 @@ list_remote_files = function(sshcon, path) {
 }
 
 #' @export
-update_campbell = function(obj, site, sshcon, remote_path) {
+update_campbell = function(outdir, sshcon, remote_path) {
   remote_files = list_remote_files(sshcon, remote_path)
   ## we only want the 'Table1' files
   remote_files = remote_files[grep('Table1', remote_files)]
-  raw_campbell_glob = file.path(attr(obj, 'raw_dir'), site, 'measurements', 'campbell', '*', '*')
+  raw_campbell_glob = file.path(outdir, '*', '*')
   old_files = Sys.glob(raw_campbell_glob)
   new_files = remote_files[!remote_files %in% basename(old_files)]
   if (length(new_files) > 0) {
     new_file_years = substring(gsub('^.*Table1_', '', new_files), 1, 4)
     new_remote_paths = file.path(remote_path, new_files)
-    new_local_paths = file.path(attr(obj, 'raw_dir'), site, 'measurements', 'campbell',
-                                new_file_years)
+    new_local_paths = file.path(outdir, new_file_years)
     for (f in unique(new_local_paths)) {
       dir.create(f, showWarnings = FALSE, recursive = TRUE)
     }
